6e0dc8b3b611f01f99e245e14f1d9911e4025d02
Lewis Nanson <148724417+LewisNanson@users.noreply.github.com>
2023-10-24 04:34:38 -0400

    Add main and encode functions

---
+++ main.py
@@ -0,0 +1,37 @@
+saved_password = ''
+
+def encode(password):
+    """Encodes a password and stores a given password"""
+    global saved_password  # this function will access the variable in the global namespace
+    saved_password = [(int(token) + 3) for token in password]  # turn every character into a digit and increase by 3
+    saved_password = str(saved_password)
+
+
+def decode():
+    """Decodes and returns the stored password"""
+    raise NotImplementedError
+
+
+def main():
+    while True:
+        print('Menu')
+        print('-' * 13)
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        print('')
+
+        user_selection = input('Please enter an option: ')
+        if user_selection == 1:
+            user_password = input('Please enter your password to encode: ')
+            encode(user_password)
+            print('Your password has been encoded and stored!')
+        elif user_selection == 2:
+            print('The encoded password is ' + saved_password + ', and the original password is ' + decode() + '.')
+        elif user_selection == 3:
+            break
+        else:
+            print('Invalid selection.')
+
+if __name__ == '__main__':
+    main()
